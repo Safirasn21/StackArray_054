4719b85d548aefbadb17dea80ab44ccb81d3ab3c
safirasn21 <safirasn021@gmai.com>
2023-06-16 16:54:00 +0700

    menambahkan fungsi display/menampilkan

--- StackusingArray/StackusingArray.cpp
+++ StackusingArray/StackusingArray.cpp
@@ -48,5 +48,11 @@ public:
 		if (empty()) {
 			cout << "\nStack is empty." << endl;
 		}
+		else {
+			for (int tmp = top; tmp >= 0; tmp--) {
+				cout << stack_array[tmp] << endl;
+			}
+		}
+	}
 
 };
